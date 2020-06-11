860547e0b0f7f35b5d6352703d5ea6e61c36612a
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-06-11 13:26:32 -0300

    Cronometro base

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+import Chronometer from './components/Chronometer'
+
+class App extends Component {
+
+    render() {
+        return (
+            <>
+                <h1>Chronometer</h1>
+                <Chronometer />
+            </>
+        )
+    }
+}
+
+
+export default App;
